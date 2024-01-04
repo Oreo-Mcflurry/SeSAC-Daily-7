0f2f349c6d5298d0df6968c8c072b3e1fea4156f
유인호 <yuiop1029@naver.com>
2024-01-04 19:50:34 +0900

    add UserDefaults

--- SeSAC Daily 7/EmotionViewController.swift
+++ SeSAC Daily 7/EmotionViewController.swift
@@ -9,20 +9,33 @@ import UIKit
 
 class EmotionViewController: UIViewController {
 	@IBOutlet var emotionLabel: [UILabel]!
-	
+
+	var emotions = [0,0,0,0,0,0,0,0,0]
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		navigationItem.leftBarButtonItem?.image = UIImage(systemName: "list.bullet")
 		self.view.backgroundColor = UIColor(patternImage: .back)
+		emotions = UserDefaults.standard.array(forKey: "emotions") as? [Int] ?? [0,0,0,0,0,0,0,0,0]
 		designLabel()
 	}
 
-	// 원래라면 UserDefaults나 Coredata같은걸 사용했어야하지만...
 	@IBAction func pressButton(_ sender: UIButton) {
-		let char = emotionLabel[sender.tag].text?.filter { $0.isLetter } ?? ""
-		let number = Int(emotionLabel[sender.tag].text?.filter { $0.isNumber } ?? "") ?? 0
+//		let char = emotionLabel[sender.tag].text?.filter { $0.isLetter } ?? ""
+//		let number = Int(emotionLabel[sender.tag].text?.filter { $0.isNumber } ?? "") ?? 0
+//
+//		emotionLabel[sender.tag].text = "\(char) \(number+1)"
 
-		emotionLabel[sender.tag].text = "\(char) \(number+1)"
+//		for index in 0..<emotionLabel.count {
+//			if emotionLabel[index] == sender {
+//				emotions[index] += 1
+//				break
+//			}
+//		}
+		emotions[sender.tag] += 1
+		UserDefaults.standard.setValue(emotions, forKey: "emotions")
+		designLabel()
+
 	}
 	@IBAction func tabGesture(_ sender: Any) {
 		view.endEditing(true)
@@ -32,7 +45,7 @@ class EmotionViewController: UIViewController {
 		let texts = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "실망해", "미안해"]
 		var index = 0
 		for lb in emotionLabel {
-			lb.text = "\(texts[index]) 0"
+			lb.text = "\(texts[index]) \(emotions[index])"
 			lb.textAlignment = .center
 			index += 1
 		}
